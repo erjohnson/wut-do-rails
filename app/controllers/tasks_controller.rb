eb10a2afeabd5e682e03ae1f848570826978f03c
Eric <erjohnson.pdx@gmail.com>
2014-08-23 16:51:03 -0700

    Add task create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,13 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render('tasks/index.html.erb')
   end
+
+  def create
+    @task = Task.new(params[:task])
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/index.html.erb')
+    end
+  end
 end
