5d6f62fd290f4114274741c4b05d88c5da63ec8c
Eric <erjohnson.pdx@gmail.com>
2014-08-23 20:38:39 -0700

    Add show task route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
   match('tasks/new', {:via => :get, :to => 'tasks#new'})
   match('tasks', {:via => :post, :to => 'tasks#create'})
+  match('tasks/:id', {:via => :get, :to => 'tasks#show'})
 end
