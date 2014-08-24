c6009ef10604a762695778264a33161806429eb1
Eric <erjohnson.pdx@gmail.com>
2014-08-23 20:52:05 -0700

    Add spec for find unfinished tasks

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 describe Task do
   it { should validate_presence_of :name }
 
-  describe '#done' do
+  describe '.done' do
     it 'should return completed tasks' do
       task1 = Task.create(name: 'Empty trash', done: true)
       task2 = Task.create(name: 'Go to Mars', done: false)
       expect(Task.done).to eq [task1]
     end
   end
+
+  describe '.not_done' do
+    it 'should return uncompleted tasks' do
+      task1 = Task.create(name: 'Empty trash', done: true)
+      task2 = Task.create(name: 'Go to Mars', done: false)
+      expect(Task.not_done).to eq [task2]
+    end
+  end
+
 end
