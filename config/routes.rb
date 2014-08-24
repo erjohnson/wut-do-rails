bee0582e0102561f57faa653009930a6816388c5
Eric <erjohnson.pdx@gmail.com>
2014-08-23 20:57:58 -0700

    Add update task route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   match('tasks', {:via => :post, :to => 'tasks#create'})
   match('tasks/:id', {:via => :get, :to => 'tasks#show'})
   match('tasks/:id/edit', {:via => :get, :to => 'tasks#edit'})
+  match('tasks/:id', {:via => [:patch, :put], :to => 'tasks#update'})
 end
