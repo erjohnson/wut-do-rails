d297ef657cfe041f0d326ab0af67b41d6ca2ca1f
Eric <erjohnson.pdx@gmail.com>
2014-08-23 19:38:14 -0700

    Add tasks/new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
   match('tasks', {:via => :post, :to => 'tasks#create'})
 end
