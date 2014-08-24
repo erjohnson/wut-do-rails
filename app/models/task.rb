e5ca6266c21dd047d5ed7b00dd59fa49372f34bb
Eric <erjohnson.pdx@gmail.com>
2014-08-23 20:50:52 -0700

    Add find done tasks method

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   validates :name, :presence => true
+
+  scope :done, -> { where(done: true) }
+  scope :not_done, -> { where(done: false) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe Task do
   it { should validate_presence_of :name }
+
+  describe '#done' do
+    it 'should return completed tasks' do
+      task1 = Task.create(name: 'Empty trash', done: true)
+      task2 = Task.create(name: 'Go to Mars', done: false)
+      expect(Task.done).to eq [task1]
+    end
+  end
 end
